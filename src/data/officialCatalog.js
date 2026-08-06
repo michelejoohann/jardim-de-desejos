ad4d06d0928252d9690eee82ea332342a20a569b
Michèlé Joohann <michelejoohann@gmail.com>
2026-08-06 10:38:11 -0300

    chore: atualizar imagens de produtos do Jardim

--- src/data/officialCatalog.js
+++ src/data/officialCatalog.js
@@ -21,6 +21,18 @@ const productOverrides = {
     priceLabel: 'R$ 327,35',
     imageUrl: 'https://m.media-amazon.com/images/I/71PLyKKNd5L._AC_SX679_.jpg',
     url: 'https://www.amazon.com.br/dp/B0H18GQLFQ?th=1&psc=1'
+  },
+  'suporte-planta-macrame-boho-cru': {
+    imageUrl: 'https://m.media-amazon.com/images/I/51DXVVqTIuL._AC_SY879_.jpg'
+  },
+  'caixa-areia-jelplast-sandbox-premium': {
+    imageUrl: 'https://p16-oec-sg.ibyteimg.com/tos-alisg-i-aphluv4xwc-sg/ca562803bd6e4292b9af295b1690346e~tplv-aphluv4xwc-crop-webp:1024:1024.webp?dr=15592&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=e1be8f53&idc=my2&from=2378011839'
+  },
+  'kit-lencol-soft-plush-felpudo-4-cores': {
+    imageUrl: 'https://p16-oec-sg.ibyteimg.com/tos-alisg-i-aphluv4xwc-sg/002a1160be61439d9cd984bf5b11c6a2~tplv-aphluv4xwc-crop-webp:1333:1333.webp?dr=15592&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=e1be8f53&idc=my2&from=2378011839'
+  },
+  'kit-cortador-grama-sem-fio-2-baterias': {
+    imageUrl: 'https://p16-oec-sg.ibyteimg.com/tos-alisg-i-aphluv4xwc-sg/921fb05e2d034e46b6d734bb38afe376~tplv-aphluv4xwc-crop-webp:1024:1024.webp?dr=15592&t=555f072d&ps=933b5bde&shp=8dbd94bf&shcp=e1be8f53&idc=my2&from=2378011839'
   }
 };
 
